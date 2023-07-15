1c103d97545289aee54fab89508d9d930adf7b6d
MajharolTanvir <majharul5456@gmail.com>
2023-07-15 18:03:02 +0600

    Footer setup done

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Footer() {
+  return (
+    <footer className="footer footer-center p-4 absolute bottom-0 bg-slate-800 text-white">
+      <div>
+        <p>
+          Copyright © 2023 - All right reserved by{" "}
+          <span className="text-cyan-500 font-medium">BookCatalog</span>
+        </p>
+      </div>
+    </footer>
+  );
+}
