31ba8570de60d7924976f78a7306d7c8609d7a56
MajharolTanvir <majharul5456@gmail.com>
2023-07-15 20:30:53 +0600

    10 Books shown on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import Slider from "../components/Slider";
+import BookDetails from "../components/BookDetails";
 
 
 export default function Home() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    void fetch("./data.json")
+      .then((res) => res.json())
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      .then((data) => setBooks(data));
+  }, []);
+
+  console.log(books);
+
   return (
     <section>
       <Slider></Slider>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 py-5 mx-auto">
+          {books?.slice(0,10).reverse().map((book) => (
+            <BookDetails book={book}></BookDetails>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
