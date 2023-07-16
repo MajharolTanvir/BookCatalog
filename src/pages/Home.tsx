d6cd066353546814a60c3dd4bcc000cee1e3aa63
MajharolTanvir <majharul5456@gmail.com>
2023-07-16 22:28:34 +0600

    Get 10 datas on home pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Slider from "../components/Slider";
 import BookCard from "../components/BookCard";
-import { useGetAllBooksQuery } from "../redux/features/bookSlice.ts/BookApi";
 import { IBook } from "../Types/GlobalTypes";
+import { useGetAllBooksQuery } from "../redux/features/bookSlice.ts/BookApi";
+
+export interface IBooksData {
+  statusCode: 200;
+  success: true;
+  message: "Books retrieved successfully!";
+  data: IBook[];
+}
 
 export default function Home() {
-  const { data }: { data?: IBook[] } = useGetAllBooksQuery(undefined);
-  console.log(data)
+  const { data }: { data?: IBooksData } = useGetAllBooksQuery(undefined);
+  console.log(data);
 
   return (
     <section>
       <Slider></Slider>
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 py-5 mx-auto">
-          {data
+          {data?.data
             ?.slice(0, 10)
             .reverse()
             .map((book: IBook) => (
